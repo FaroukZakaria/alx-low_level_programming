96288f9419c3e2921d4320949ed468bda7c3c7f1
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-02-16 22:49:15 +0000

    hello world

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+	char C;
+	int I;
+	long L;
+	long long LL;
+	float F;
+	printf("Size of a char: %zu byte(s)", sizeof(C));
+	printf("Size of an int: %zu byte(s)", sizeof(I));
+	printf("Size of a long int: %zu byte(s)", sizeof(L));
+	printf("Size of a long long int: %zu byte(s)", sizeof(LL));
+	printf("Size of a float: %zu byte(s)", sizeof(F);
+			return (0);
+			}
+	
