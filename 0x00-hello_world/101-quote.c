f369cd0a972386dc753bcccaef836cd6fb9b0372
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-02-16 23:24:24 +0000

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-/*
-*main - Entry point
-*Return: Always 1 (Success)
-*/
+#include <unistd.h>
 
-const char* ans() {
-	return "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-}
+/**
+ * *main - Entry point
+ * *Return: Always 1
+ * */
 
 int main(void)
 {
-	return (ans);
-	return (1);
+		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+			return (1);
 }
