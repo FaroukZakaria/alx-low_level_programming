7d110aef2e50ad2cc8f40ea68540773162051203
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-02-16 23:18:24 +0000

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,7 @@
 
 int main(void)
 {
-	return("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	char [ans] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+		return (ans);
 	return (1);
 }
