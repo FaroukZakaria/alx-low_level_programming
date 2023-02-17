b22ef9681f0d9408d40543f68cfc0fd14d354c36
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-02-17 23:40:42 +0000

    C - if else while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+		for (n = 48; n < 58; n++)
+		{
+			putchar(n);
+		}
+		for (n = 65; n < 91; n++)
+		{
+			putchar(n);
+		}
+		putchar('\n');
+		return (0);
+}
