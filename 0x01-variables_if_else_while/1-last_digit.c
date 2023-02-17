90687ba06196290778f249e1f5cee392dc8abe60
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-02-17 22:00:41 +0000

    C - if else while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,7 +13,7 @@ int main(void)
 	int nl;
 		srand(time(0));
 		n = rand() - RAND_MAX / 2;
-		nl == n % 10;
+		nl = n % 10;
 		if (nl == 0)
 		{
 			printf("Last digit of %d is %d and is 0\n", n, nl);
