332d13d48d76699b96fb7459e0898ed303552a23
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-02-21 23:00:48 +0000

    nested loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_sign - hoi
+ * @n: ni-
+ * Return: 1 or 0 or "-1"
+ */
+int print_sign(int n);
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		return (0);
+		_putchar('0');
+	}
+	return (-1);
+	_putchar('-');
+}
