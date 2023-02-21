4bb6e17e061465d56a3f7da5f53ea4ab0549ee0c
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-02-21 22:34:17 +0000

    nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - hoi
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+		for (j = 0; j < 10;)
+		{
+			for (i = 'a'; i <= 'z'; i++)
+			{
+				_putchar(i);
+			}
+			_putchar('\n');
+			j++;
+		}
