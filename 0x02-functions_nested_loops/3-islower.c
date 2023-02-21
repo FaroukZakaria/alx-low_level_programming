8cfffbb1f0cd5121eb868c3df0efa0708691f185
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-02-21 22:47:14 +0000

    nested loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
  * _islower - omg
- * Return: void
+ * Return: 1 for true 0 for false
  */
 int _islower(int c)
 {
-	if (c == islower(c))
+	if (c >= 97 && c <== 122)
 	{
 		return (1);
 	}
