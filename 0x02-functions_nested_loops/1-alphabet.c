19ca476db313e3db0238a4aa8bb86cbd0bb589d5
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-02-21 22:18:20 +0000

    nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+		for (i = 'a', i <= 'z', i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+		return (0);
+}
