246858432ae513c671fc1857dc131cc37a84be71
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-02-22 22:35:44 +0000

    0x02 C

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - hoi
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	long j = 1, k = 2;
+		for (i = 0; i <= 50; i++)
+		{
+			if (i == 0)
+			{
+				printf("%d, ", i);
+			}
+			else if (i == 1)
+			{
+				printf("%d, ", i);
+			}
+			else
+			{
+				k += j;
+				j = k - j;
+				printf("%d, ", k);
+			}
+		}
+		printf("\n");
+		return (0);
+}
