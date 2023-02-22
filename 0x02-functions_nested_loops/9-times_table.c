89fa1d9d2a97405bbe155807af47cc838f0a5260
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-02-22 01:15:32 +0000

    nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,12 +6,12 @@
 void times_table(void)
 {
 	int x, y, z, u, d;
-		for (x = 48; x < 58; x++)
+		for (x = 0; x <= 9; x++)
 		{
-		for (y = 48; y < 58; y++)
+		for (y = 0; y <= 9; y++)
 		{
 		z = x * y;
-		if (z > 57)
+		if (z > 9)
 		{
 		u = z % 10;
 		d = (z - u) / 10;
@@ -28,7 +28,7 @@ void times_table(void)
 		_putchar(32);
 		_putchar(32);
 		}
-		_putchar(z);
+		_putchar('0' + z);
 		}
 		}
 		_putchar('\n');
