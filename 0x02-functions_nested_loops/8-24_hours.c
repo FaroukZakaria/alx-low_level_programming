1578f269b0013a94506f33eaf136e735608d35b3
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-02-22 00:28:11 +0000

    nested loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,19 +9,24 @@ void jack_bauer(void)
 	int a, b, c, d;
 		for (a = 48; a < 51; a++)
 		{
+			_putchar (a);
 			for (b = 48; b < 52; b++)
 			{
+				_putchar (b);
+				_putchar (':');
 				for (c = 48; c < 54; c++)
 				{
+					_putchar (c);
 					for (d = 48; d < 58; d++)
 					{
 						_putchar (d);
+						_putchar ('\n');
+						break
 					}
-					_putchar (c);
+					break
 				}
-				_putchar (':');
-				_putchar (b);
+				break
 			}
-			_putchar (a);
+			break
 		}
 }
