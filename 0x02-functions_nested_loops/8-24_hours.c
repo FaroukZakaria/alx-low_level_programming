149f8596a76d6fab76d0bbd9e9a172867e8847e3
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-02-22 00:14:15 +0000

    nested loops

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * jack_bauer - hoi
+ * @a: hoi
+ * Return: do i need it?
+ */
+void jack_bauer(void)
+{
+	int a;
+		for (a = 48; a < 51; a++)
+		{
+			for (b = 48; b < 52; b++)
+			{
+				for (c = 48; c < 54; c++)
+				{
+					for (d = 48; d < 58; d++)
+					{
+						_putchar (d);
+					}
+					_putchar (c);
+				}
+				_putchar (':');
+				_putchar (b);
+			}
+			_putchar (a);
+		}
+}
