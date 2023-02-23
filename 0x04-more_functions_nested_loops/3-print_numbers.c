4377bb8711e89601cd56d207584add8562a1836d
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-02-23 15:05:32 +0000

    task answer

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - hoi
+ * Return: nothing
+ * @i: hoi
+ */
+void print_numbers(int i)
+{
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
