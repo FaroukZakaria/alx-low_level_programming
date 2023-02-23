2adf540f06229ab08d5089e984814c1d69a95cb9
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-02-23 15:34:55 +0000

    task answer

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,12 +4,13 @@
  */
 void print_most_numbers(void)
 {
-	for (int i = 48; i < 58; i++)
-	{
-		if (i != 50 && i != 52)
+	int i;
+		for (i = 48; i < 58; i++)
 		{
-			_putchar(i);
+			if (i != 50 && i != 52)
+			{
+				_putchar(i);
+			}
 		}
-	}
-	_putchar('\n');
+		_putchar('\n');
 }
