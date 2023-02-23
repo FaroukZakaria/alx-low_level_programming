29f33b3534987f36df01265170a21cd212f8810d
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-02-23 17:45:22 +0000

    task answer

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,20 +1,24 @@
 #include "main.h"
 /**
  * print_square - f
+ * @size: asd
  */
 void print_square(int size)
 {
 	int i, j;
 		for (i = 1; i <= size; i++)
 		{
-			for (j = 1; j <= size; j++)
+			if (size > 0)
 			{
-				if (size > 0)
+				for (j = 1; j <= size; j++)
 				{
-					_putchar('#');
-				}
-				{
-					continue;
+					if (size > 0)
+					{
+						_putchar('#');
+					}
+					{
+						continue;
+					}
 				}
 			}
 			_putchar('\n');
