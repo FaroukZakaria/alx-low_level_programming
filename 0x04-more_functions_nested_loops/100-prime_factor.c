2dbc54672301aab0aff4c56c4043de88113e1cf9
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-02-23 19:47:42 +0000

    task answer

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	long i, p, n;
+	double s; sqrt(n);
+		for (i = 1; i <= s; i++)
+		{
+			if (n % i == 0)
+			{
+				p = n / i;
+			}
+		}
+		printf("%ld\n", p);
+		return (0);
+}
