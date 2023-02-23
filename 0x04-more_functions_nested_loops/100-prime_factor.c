ecd490e571af83c4a36373d8f5ef831782bc4741
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-02-23 19:53:40 +0000

    task answer

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -9,13 +9,13 @@ int main(void)
 	long i, p;
 	long n = 612852475143;
 	double s =  sqrt(n);
-		for (i = 1; i <= s; i++)
-		{
-			if (n % i == 0)
+			for (i = 1; i <= s; i++)
 			{
-				p = n / i;
+				if (n % i == 0)
+				{
+					p = n / i;
+				}
 			}
-		}
-		printf("%ld\n", p);
-		return (0);
+			printf("%ld\n", p);
+			return (0);
 }
