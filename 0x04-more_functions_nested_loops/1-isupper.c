d10205805bfb3777e96d72616f0499aa63e57fb5
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-02-23 14:55:17 +0000

    task answer

---
+++ 0x04-more_functions_nested_loops/1-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit - hoi
+ * Return: 0 and 1
+ * @c: let's not forget this time
+ */
+int _isdigit(int c)
+{
+	if (c > 48 && c < 58)
+	{
+		return (1);
+	}
+	return (0);
+}
