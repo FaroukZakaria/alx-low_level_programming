8415d9b91037555031efa3f563854095215373d3
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-02-23 15:46:06 +0000

    task answer

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * more_numbers - .
+ */
+void more_numbers(void)
+{
+	int i, j;
+		for (i = 0; i < 10; i++)
+		{
+			for (j = 48; j < 48; j++)
+			{
+				_putchar(j);
+			}
+			_putchar('\n');
+		}
+}
