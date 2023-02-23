7b246fa3ac1e16a2fd97c24e80842b578ad5579a
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-02-23 17:05:36 +0000

    task answer

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,10 +11,10 @@ void more_numbers(void)
 			{
 				if (j > 9)
 				{
-					_putchar('0' + (j / 10));
+					_putchar((j / 10) + '0');
 				}
 				{
-					_putchar('0' + (y % 10));
+					_putchar((j % 10) + '0');
 				}
 			}
 			_putchar('\n');
