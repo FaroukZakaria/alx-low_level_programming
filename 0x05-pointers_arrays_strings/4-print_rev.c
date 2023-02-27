3406e7b8b168b9f320472c8306a8fd0daf96b722
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-02-27 21:38:52 +0000

    pointer&arrays ans

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,8 +5,14 @@
  */
 void print_rev(char *s)
 {
-	for (s = '\0'; s > 0; s--)
-	{
-		_putchar(*s);
-	}
+	int c = 0;
+		while (s[c] != '\0')
+		{
+			c++;
+		}
+		for (c -= 1; c >= 0; c--)
+		{
+			_putchar(s[c]);
+		}
+		_putchar('\n');
 }
