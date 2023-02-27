c5805df8c340b46743d4c3005c56643b14663516
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-02-27 22:19:53 +0000

    pointer&arrays ans

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts2 - p
+ * @str: str
+ */
+void puts2(char *str)
+{
+	int i, j;
+		for (i = 0; i != '\0';)
+		{
+			i++;
+		}
+		for (j = 0; j < i; j += 2)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+}
