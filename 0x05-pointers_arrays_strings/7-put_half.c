adef257936645d158c447f5590463a6d9b194671
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-02-27 22:28:57 +0000

    pointer&arrays ans

---
+++ 0x05-pointers_arrays_strings/7-put_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * puts_half - p
+ * @str: s
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+		while (str[i++])
+		{
+			i++;
+		}
+		if (i % 2 == 0)
+		{
+			for (*str = i / 2; str[i] != '\0'; *str++)
+			{
+				_putchar(str[i]);
+			}
+		}
+		else
+		{
+			for (*str = (i - 1) / 2; str[i] != '\0'; *str++)
+			{
+				_putchar(str[i]);
+			}
+		}
+		_putchar('\n');
+}
