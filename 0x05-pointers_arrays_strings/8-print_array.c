7745b767774d8d1dc49d6e42af46182a0df27b0d
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-02-27 22:51:41 +0000

    pointer&arrays ans

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array - v
+ * @a: a
+ * @n: ni-
+ */
+void print_array(int *a, int n)
+{
+	int i;
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", *a[i]);
+			if (i < n - 1)
+			{
+				printf(", ");
+			}
+		}
+}
