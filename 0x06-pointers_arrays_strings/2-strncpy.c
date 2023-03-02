3823b53a6b07d03a21838f1089062d46e518e500
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-03-02 15:47:53 +0000

    task answer

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * strncpy - hoi
+ * @dest: hoi
+ * @src: hoi
+ * @n: ni-
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a;
+		for (a = 0; src[a] && a < n; a++)
+		{
+			dest[a] = src[a];
+		}
+		return (dest);
+}
