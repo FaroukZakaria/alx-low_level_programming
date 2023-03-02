ea2c6e268674da45bb465576b9b1e6bfc250e925
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-03-02 23:50:00 +0000

    task answer

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * leet - yeet
+ * @str: s
+ * Return: ni-
+ */
+char *leet(char *str)
+{
+	int a = 0, b;
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', '!'};
+		while (str[a])
+		{
+			for (b = 0; b <= 7; b++)
+			{
+				if (str[a] == leet[b] ||
+				str[a] - 32 == leet[b])
+					str[a] = b + '0';
+			}
+			a++;
+		}
+		return (str);
+}
