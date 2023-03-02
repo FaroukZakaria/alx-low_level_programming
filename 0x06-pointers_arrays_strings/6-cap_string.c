ab61d0b593b735b8a35de92641e3b41bcc4a8172
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-03-02 23:03:06 +0000

    task answer

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,13 +8,13 @@ char *cap_string(char *str)
 {
 	int a = 0;
 
-		while (str[a])
-		{
-		while (!(str[a] >= 'a' && str[a] <= 'z'))
-		a++;
-		if ((str[a - 1] == ' ' || str[a - 1] == '\t' || str[a - 1] == '\n' || str[a - 1] == ',' || str[a - 1] == ';' || str[a - 1] == '.' || str[a - 1] == '!' || str[a - 1] == '?' || str[a - 1] == '"' || str[a - 1] == '(' || str[a - 1] == ')' || str[a - 1] == '{' || str[a - 1] == '}' || a == 0))
-		str[a] -= 32;
-		a++;
-		}
-		return (str);
+	while (str[a])
+	{
+	while (!(str[a] >= 'a' && str[a] <= 'z'))
+	a++;
+	if (str[a - 1] == ' ' || str[a - 1] == '\t' || str[a - 1] == '\n' || str[a - 1] == ',' || str[a - 1] == ';' || str[a - 1] == '.' || str[a - 1] == '!' || str[a - 1] == '?' || str[a - 1] == '"' || str[a - 1] == '(' || str[a - 1] == ')' || str[a - 1] == '{' || str[a - 1] == '}' || a == 0)
+	str[a] -= 32;
+	a++;
+	}
+	return (str);
 }
