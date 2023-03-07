e170ef5ee26202e15c208742a5b75b66ce43b9f8
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-03-07 20:14:01 +0000

    task answer

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _memcpy - meme omg
+ * @dest: d
+ * @src: again...
+ * @n: ni-
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+		for (i = 0; i != n; i++)
+		{
+			dest[i] = src[i];
+		}
+		return (dest);
+}
