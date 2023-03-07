d272fd730edd89be612798a41b04c9b5dea0cfc3
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-03-07 20:23:47 +0000

    task answer

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strchr - s
+ * @s: s
+ * @c: c
+ * Return: s
+ */
+char *_strchr(char *s, char c)
+{
+	int i, j;
+		while (s++)
+		{
+			j++;
+		}
+		for (i = 0; i < j; i++)
+		{
+			if (s[i] == 'c')
+			{
+				k = i;
+				break;
+			}
+			if ((i = j) && k != i)
+			{
+				k = 'NULL'
+			}
+		}
+	return (s);
+}
