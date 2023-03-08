485c8419a1e114ecee1ad60e06c95a1ac3beadd9
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-03-08 16:58:52 +0000

    task answer

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,13 +2,12 @@
 /**
  * _puts_recursion - s
  * @s: s
- * @i: i
  */
-void _puts_recursion(char *s, int i = 0)
+void _puts_recursion(char *s)
 {
 	if (s[i] != '\0')
 	{
-		_putchar(s[i]);
-		_puts_recursion(s, i + 1);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,5 @@
 void _putchar();
-void _puts_recursion(char *s, int i);
+void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
