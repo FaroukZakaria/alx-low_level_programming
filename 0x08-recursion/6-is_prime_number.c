999f4d32ee4fe69735fede4cbbc2e5f0f601d2cb
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-03-08 20:23:24 +0000

    task answer

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -18,9 +18,9 @@ int check(int n, int i)
 	{
 		return (1);
 	}
-	return check(n, i + 1);
+	return (check(n, (i + 1)));
 }
 int is_prime_number(int n)
 {
-        return (check(n, 1));
+	return (check(n, 1));
 }
