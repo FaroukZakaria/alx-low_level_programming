2183d477bb7b0ba599d0a8e8e4e965274b6a41f5
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-03-08 20:20:39 +0000

    task answer

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * is_prime_number - i
+ * @n: ni-
+ * Return: 0, 1
+ */
+int is_prime_number(int n)
+{
+	return (check(n, 1));
+}
+int check(int n, int i)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else if ((n % i) == 0 && (i > 1))
+	{
+		return (0);
+	}
+	else if ((n / i) < i)
+	{
+		return (1);
+	}
+	return check(n, i + 1);
+}
