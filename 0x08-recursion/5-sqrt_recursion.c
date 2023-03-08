af8d6cc5cd087fa393e05ed2baba50ed2c5d9bd5
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-03-08 20:00:38 +0000

    task answer

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * finder - f
+ * @n: ni-
+ * @i: i
+ * Return: boop
+ */
+int finder(n, i)
+{
+	int sqrt i * i;
+		if (sqrt > n)
+		{
+			return (-1);
+		}
+		if (sqrt == n)
+		{
+			return (i);
+		}
+		return (finder(n, i + 1));
+}
+/**
+ * _sqrt_recursion - s
+ * @n: ni-
+ * Return: ni-
+ */
+int _sqrt_recursion(int n)
+{
+	return (finder(n, 1));
+}
