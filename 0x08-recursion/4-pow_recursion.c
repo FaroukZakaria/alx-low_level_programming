daf6b64b2d8ecd85a4153c1006366c52a08410aa
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-03-08 19:06:44 +0000

    task answer

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _pow_recursion - a
+ * @x: x
+ * @y: y
+ * Return: -1
+ */
+int _pow_recursion(int x, int y)
+{
+	int i;
+		if (y < 0)
+		{
+			return (-1);
+		}
+		i = x ^ y;
+		return (i);
+}
