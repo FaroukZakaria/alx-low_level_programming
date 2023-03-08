9db84217f120f3be93effdc7fb8dc50cdc13e577
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-03-08 20:45:39 +0000

    task answer

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * _len - l
+ * @s: su-
+ * Return: r
+ */
+int _len(char *s)
+{
+	if (*s)
+	{
+		return (1 + _len(s + 1));
+	}
+	else
+	{
+		return (0);
+	}
+/**
+ * iter - i
+ * @s: su-
+ * @i: i
+ * @j: j
+ * Return: .
+ */
+int iter(char *s, int i, int j)
+{
+	if (*(s + i) == *(s + j))
+	{
+		if (n1 == n2 || n1 == n2 + 1)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
+/**
+ * is_palindrome - i
+ * @s: s
+ * Return: 0, 1
+ */
+int is_palindrome(char *s)
+{
+	if (*s)
+	{
+		return (iter(s, 0, _len(s) - 1));
+	}
+	else
+	{
+		return (1);
+	}
+}
