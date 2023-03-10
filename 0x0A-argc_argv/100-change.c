03ddc9a36fda3b83e01e96abe999ffe6afe370aa
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-03-10 21:29:30 +0000

    task answer

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -18,11 +18,6 @@ int main(int argc, char *argv[])
 			return (1);
 		}
 		coins = atoi(argv[1]);
-		if (argv[1][0] == '-')
-		{
-			printf("0\n");
-			return (0);
-		}
 		while (coins > 0)
 		{
 			total++;
