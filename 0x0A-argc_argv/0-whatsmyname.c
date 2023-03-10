becce024b1f60f475acc630ed11930b801d54026
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-03-10 18:21:24 +0000

    task answer

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - e
+ * Return: 0
+ */
+int main(int argc, argv[])
+{
+	prtinf("%d\n", argc);
+	_putchar(argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+gotcha dis time
