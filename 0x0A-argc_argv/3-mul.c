70f452807cd29d7805620113b88832d6f7772c39
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-03-10 18:44:58 +0000

    task answer

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - e
+ * @argc: a
+ * @argv: a
+ * Return: 0, 1
+ */
+int main(int argc, char *argv[])
+{
+	int i, mul;
+		if (argc > 1)
+		{
+			for (i = 1, mul = 1; i < argc; i++)
+			{
+				mul *= atoi(argv[i]);
+			}
+			printf("%d\n", mul);
+			return (0);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+}
