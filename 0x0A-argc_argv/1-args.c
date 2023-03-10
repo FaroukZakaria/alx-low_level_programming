22c274c80c0d1b58a4b1fbe5a16eb2f724d99e8f
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-03-10 18:29:59 +0000

    task answer

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - e
+ * Return: 0
+ * @argc: a
+ * @argv: a
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
