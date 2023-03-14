df047fd5ae5da00addae5e195f0092ec0166ab10
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-03-14 15:48:45 +0000

    task answer

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * create_array - a
+ * @size: s
+ * @c: c
+ * Return: and leaves, just like yo da-
+ */
+char *create_array(unsigned int size, char c)
+{
+	char array[]
+	int i;
+		for (i = 0; i < size; i++)
+		{
+			char array[i] = c;
+		}
+}
