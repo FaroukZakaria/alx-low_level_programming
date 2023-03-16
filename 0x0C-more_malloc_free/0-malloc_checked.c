c979e27b5ddf7122355d64a5acc723a515d52545
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-03-16 15:41:15 +0000

    task answer

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - s
+ * @b: s
+ */
+void *malloc_checked(unsigned int b)
+{
+	if (unsigned int b)
+	{
+		malloc(unsigned int b);
+	}
+	else
+	{
+		exit(98);
+	}
+}
