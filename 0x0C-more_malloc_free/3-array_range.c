0cd33df4b7e228c166f69bf4fb750ae40f035417
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-03-16 22:54:45 +0000

    task answer

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,12 +12,13 @@ int *array_range(int min, int max)
 	char *i;
 		if (min > max)
 			return (NULL);
-		i = malloc(sizeof(int) * max);
+		i = malloc((sizeof(int) * (max - min)) + sizeof(int));
 		if (i == NULL)
 			return (NULL);
 		while (min < max)
 		{
 			i[j] = min;
+			j++;
 			min++;
 		}
 		return (i);
