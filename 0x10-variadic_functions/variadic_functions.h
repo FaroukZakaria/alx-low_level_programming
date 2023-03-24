4ecb51678d3b464d1349778d27d80665fd970d87
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-03-24 23:43:14 +0000

    task answer

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,5 +6,5 @@ int sum_them_all(const unsigned int n, ...);
 int sum_them_all(const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void _putchar();
+void _putchar(void);
 #endif
