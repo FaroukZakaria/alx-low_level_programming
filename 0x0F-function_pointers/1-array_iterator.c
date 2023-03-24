5a8bbe6ab14fa1e5b6d612d76546457cf404761a
FaroukZakaria <123017280+FaroukZakaria@users.noreply.github.com>
2023-03-24 14:14:01 +0200

    The capital a!! It wasn't a problem!!

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,7 +9,7 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i = 0;
-		if (!Array || !action)
+		if (!array || !action)
 			return;
 		for (i = 0; i < size; i++)
 			action(array[i]);
