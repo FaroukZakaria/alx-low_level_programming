216fa462230449f58b57763b80090641f11ff49f
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-03-25 00:01:49 +0000

    task answer

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,53 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_all - s
+ * @format: s
+ * Return: s
+ */
+void print_all(const char * const format, ...)
+{
+	va_list ap;
+	unsigned int i = 0, j, c = 0;
+	char *s;
+	const char t_arg[] = "cifs";
+		va_start(ap, format);
+		while (format && format[i])
+		{
+			j = 0;
+			while (t_arg[j])
+			{
+				if (format[i] == t_arg[j] && c)
+				{
+					printf(", ");
+					break;
+				}
+				j++;
+			}
+			switch (format[i])
+			{
+				case 'c':
+					printf("%c", va_arg(ap, int)), c = 1;
+					break;
+				case 'i':
+					printf("%d", va_arg(ap, int)), c = 1;
+					break;
+				case 'f':
+					printf("%f", va_arg(ap, double)), c = 1;
+					break;
+				case 's':
+					s = va_arg(ap, char *), c = 1;
+					if (!s)
+					{
+						printf("(nil)");
+						break;
+					}
+					printf("%s", s);
+					break;
+			}
+			i++;
+		}
+		va_end(ap);
+		printf("\n");
+}
