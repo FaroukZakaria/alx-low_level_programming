2953907d5d48cc01b039d7be968b684f5bd92be7
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-03-30 16:08:28 +0000

    task answer

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void __attribute__ ((constructor)) first(void);
+/**
+ * first - s
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
