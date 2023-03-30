00f8a37732ce677984071208f4559c8ff83a9064
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-03-30 16:44:35 +0000

    task answer

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,7 +12,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	char *dup;
 	int len;
 	list_t *new, *last;
-		new = malloc(sizeof (list_t));
+		new = malloc(sizeof(list_t));
 		if (new == NULL)
 		{
 			return (NULL);
