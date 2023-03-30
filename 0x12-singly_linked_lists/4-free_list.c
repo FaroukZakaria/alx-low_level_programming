250caeb99f638392ba5e101d8a4da29c45422402
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-03-30 16:04:21 +0000

    task answer

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,11 +8,12 @@
  */
 void free_list(list_t *head)
 {
-	while (head)
-	{
-		tmp = head->next;
-		free(head->str);
-		free(head);
-		head = tmp;
-	}
+	list_t *tmp;
+		while (head)
+		{
+			tmp = head->next;
+			free(head->str);
+			free(head);
+			head = tmp;
+		}
 }
