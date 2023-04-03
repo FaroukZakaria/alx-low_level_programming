26d328fa7d0f951b0eb97c3a0bfe5efff035a2ea
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-04-03 17:17:21 +0000

    task answer

---
+++ 0x13-more_singly_linked_lists/3-idk.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * add_nodeint_end - s
+ * @head: s
+ * @n: s
+ * Return: s
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new, *cur;
+	
+	new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		cur = *head;
+		while (cur)
+		{
+			cur = cur->next;
+		}
+		cur->next = new;
+	}
+	return (new);
+}
