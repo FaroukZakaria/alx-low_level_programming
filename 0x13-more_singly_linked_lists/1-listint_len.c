56464d7a78c1c0cd867ad1becdbc6fa953d37309
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-04-03 14:44:38 +0000

    task answer

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * listint_len - s
+ * @h: s
+ * Return: s
+ */
+size_t listint_len(const listint_t *h)
+{
+	int i;
+		while (h)
+		{
+			i++;
+		}
+		return (i);
+}
