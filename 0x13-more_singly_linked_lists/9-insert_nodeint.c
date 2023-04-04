5c5e7830639e0f995e7095afa4c414af1b675a08
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-04-04 17:56:34 +0000

    task answer

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,48 +9,54 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i = 0;
-	listint_t *curnew = *head;
-	listint_t *new = malloc(sizeof(listint_t));
-	listint_t *cur = *head;
-	new->n = n;
-
-	if (new == NULL)
+	if (*head == NULL || idx == 0)
 	{
 		return (NULL);
 	}
 	else
 	{
-		while (cur != NULL)
-		{
-			i++;
-			cur = cur->next;
-		}
-		if (i < idx)
+		unsigned int i = 0;
+		listint_t *curnew = *head;
+		listint_t *new = malloc(sizeof(listint_t));
+		listint_t *cur = *head;
+		new->n = n;
+
+		if (new == NULL)
 		{
 			return (NULL);
 		}
 		else
 		{
-			i = 0;
-
-			while (i < (idx - 1))
+			while (cur != NULL)
 			{
-				curnew = curnew->next;
 				i++;
+				cur = cur->next;
 			}
-			if (curnew->next != NULL)
+			if (i < idx)
 			{
-				listint_t *curr = curnew->next;
-				curnew->next = new;
-				new->next = curr;
+				return (NULL);
 			}
 			else
 			{
-				curnew->next = new;
-				new->next = NULL;
+				i = 0;
+
+				while (i < (idx - 1))
+				{
+					curnew = curnew->next;
+					i++;
+				}
+				if (curnew->next != NULL)
+				{
+					listint_t *curr = curnew->next;
+					curnew->next = new;
+					new->next = curr;
+				}
+				else
+				{
+					curnew->next = new;
+					new->next = NULL;
+				}
 			}
 		}
-	}
-	return (new);
+		return (new);
 }
