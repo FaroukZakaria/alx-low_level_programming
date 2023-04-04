20228180ea0b3db8f83fc45aeda6688b18122562
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-04-04 11:24:24 +0000

    task answer

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint - s
+ * @head: s
+ * Return: s
+ */
+void free_listint(listint_t *head)
+{
+	while (head != NULL)
+	{
+		listint_t *tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+	free(head);
+}
