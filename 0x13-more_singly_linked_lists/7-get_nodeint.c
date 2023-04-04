17b2a113b1c6c4a2f1ff24b0432d610ea2d5cb3d
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-04-04 14:10:08 +0000

    task answer

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * get_nodeint_at_index - a
+ * @head: s
+ * @index: s
+ * Return: s
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		listint_t *tmp = head;
+		unsigned int i, j;
+
+		while (tmp != NULL)
+		{
+			i++;
+			tmp = tmp->next;
+		}
+		if (index >= i)
+		{
+			return (NULL);
+		}
+		else
+		{
+			tmp = head;
+
+			for (j = 0; j <= index; j++)
+			{
+				if (j == index)
+				{
+					return (tmp);
+				}
+				tmp = tmp->next;
+			}
+		}
+	}
+	return (NULL);
+}
