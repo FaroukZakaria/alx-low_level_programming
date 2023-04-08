ea86a45fa8ce164521e4945f6763c15c01e1a260
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-04-08 15:58:31 +0000

    task answer

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,12 +10,12 @@ unsigned int binary_to_uint(const char *b)
 {
 	int i = strlen(b) - 1, sum = 0;
 	int j, k = 1;
-	
+
 	if (b == NULL)
 	{
 		return (0);
 	}
-	
+
 	for (j = 0; b[j] != '\0'; j++)
 	{
 		if (b[j] > '1' || b[j] < '0')
