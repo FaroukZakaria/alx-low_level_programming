7654b3b8a7985ff65f63cf39b81eb62e87fe489f
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-05-04 18:50:37 +0000

    task answer

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * set_bit - s
+ * @n: s
+ * @index: s
+ * Return: s
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int i = 1 << index;
+	int j;
+
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+	*n |= i;
+	j = *n;
+	return (j);
+}
