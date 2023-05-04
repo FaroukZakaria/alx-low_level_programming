3c887f6319716ab62d91ed2d69c87ef8bd83ae80
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-05-04 18:07:15 +0000

    task answer

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,7 +7,7 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int i = 1 << index;
+	int i = 1 << index;
 	int j = (n & i) >> index;
 
 	if (i > n || index >= sizeof(unsigned long int) * 8)
