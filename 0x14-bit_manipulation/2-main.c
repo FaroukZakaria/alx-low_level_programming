77d1a57e75e25436cd6c7081285ade074d070373
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-05-04 15:50:37 +0000

    task answer

--- 0x14-bit_manipulation/2-main.c
+++ 0x14-bit_manipulation/2-main.c
@@ -16,5 +16,9 @@ int main(void)
 	printf("%d\n", n);
 	n = get_bit(1024, 0);
 	printf("%d\n", n);
+	n = get_bit(3, 5);
+	printf("%d\n", n);
+	n = get_bit(1024, 11);
+	printf("%d\n", n);
 	return (0);
 }
