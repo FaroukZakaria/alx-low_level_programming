936f57ea1148a40b03912556082edf5ec4eee50b
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-05-08 21:45:44 +0000

    task answer

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,27 +12,28 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	ssize_t file, rd, wr, totalrd;
+	char buffer[SIZE];
+
 	if (filename == NULL)
 	{
 		return (0);
 	}
-	ssize_t file, rd, wr, totalrd;
-	char buffer[SIZE];
 
 	file = open(filename, O_RDONLY);
-	if (file == "-1")
+	if (file == -1)
 	{
 		return (0);
 	}
 	totalrd = 0;
 	while ((rd = read(file, buffer, SIZE)) > 0 && letters > 0)
 	{
-		if (letters < rd)
+		if ((ssize_t)letters < rd)
 		{
 			rd = letters;
 		}
 		wr = write(STDOUT_FILENO, buffer, rd);
-		if (wr == "-1" || wr != rd)
+		if (wr == -1 || wr != rd)
 		{
 			close(file);
 			return (0);
@@ -40,12 +41,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		letters -= rd;
 		totalrd += rd;
 	}
-	if (rd == "-1")
+	if (rd == -1)
 	{
 		close(file);
 		return (0);
 	}
-	if (close(file) == "-1")
+	if (close(file) == -1)
 	{
 		return (0);
 	}
