33a2e0e08eb10dd025d3941656f024bd95481d31
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-05-09 18:44:51 +0000

    task answer

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,19 +13,22 @@ int create_file(const char *filename, char *text_content)
 {
 	FILE *file2;
 
+	mode_t old_mask = umask(S_IWGRP | S_IWOTH);
+
 	file2 = fopen(filename, "wb");
 	if (file2 == NULL)
 	{
 		return (-1);
 	}
-	if (fputs(text_content, file) == EOF)
+	if (fputs(text_content, file2) == EOF)
 	{
-		fclose(file);
+		fclose(file2);
 		return (-1);
 	}
-	if (fclose(file) == EOF)
+	if (fclose(file2) == EOF)
 	{
 		return (-1);
 	}
+	umask(old_mask);
 	return (0);
 }
