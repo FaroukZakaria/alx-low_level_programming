8e19c4b3c9291c019f79a81427a7c31a2ce256cf
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-05-09 19:03:52 +0000

    task answer

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -33,7 +33,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	else
 	{
-		file = open(filename, O_WRONLY|O_CREAT, S_IRUSR|S_IWUSR);
+		file = open(filename, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
 		if (file < 0)
 		{
 			return (-1);
