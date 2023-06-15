9df985aa92c6cc56c8aa3b6857d0846a9098ebad
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-06-15 18:44:24 +0000

    task answer

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -8,15 +8,16 @@ void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *tmp;
 
-	if (tmp == NULL)
-		return (NULL);
-	while (tmp)
+	while (head && head->next != NULL)
 	{
 		tmp = head;
 		head = head->next;
 		tmp->next = NULL;
 		tmp->prev = NULL;
 		head->prev = NULL;
+		printf("freeing\n");
 		free (tmp);
 	}
+	tmp = head;
+	free (tmp);
 }
