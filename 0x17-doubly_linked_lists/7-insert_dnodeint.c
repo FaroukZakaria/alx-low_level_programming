fff64fd6c2aec0eb0242ca16568a7c6249c23cdd
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-06-15 20:40:36 +0000

    task answer

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,6 +16,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	if (*h == NULL)
 	{
+		if (idx > 0)
+			return (NULL);
 		node->n = n;
 		node->prev = NULL;
 		node->next = NULL;
