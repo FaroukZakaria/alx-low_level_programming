79c9aa5a47c738dab746f7ccda4abfe76c12d59e
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-06-15 19:10:40 +0000

    task answer

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - s
+ * @head: s
+ * @index: s
+ * Return: s
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	int i = 0;
+
+	while (i < index)
+	{
+		if (head == NULL)
+			return (NULL);
+		head = head->next;
+		i++;
+	}
+	return (head);
+}
