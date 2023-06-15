f9998284ede28ec2af65b7bd3e8d1ba2c3ef8094
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-06-15 19:13:41 +0000

    task answer

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * sum_dlistint - s
+ * @head: s
+ * Return: s
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
