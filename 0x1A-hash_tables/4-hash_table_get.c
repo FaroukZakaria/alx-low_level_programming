7aeb09fed90fa89dffd2c6129ae7f6cb6d89fd16
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-07-07 19:48:48 +0000

    task answer

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -8,11 +8,15 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int i;
+	hash_node_t *node;
 
-	for (i = 0; ht->array[i]; i++)
-	{
-		if (strcmp(ht->array[i]->key, key) == 0)
-			return (ht->array[i]->value);
-	}
-	return (NULL);
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+	i = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[i];
+	if (node == NULL)
+		return (NULL);
+	while (strcmp(node->key, key) != 0)
+		node = node->next;
+	return (node->value);
 }
