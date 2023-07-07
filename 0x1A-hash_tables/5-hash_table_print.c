c8baa74c159695a2ea66334e66a1f5caaca4742c
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-07-07 20:39:52 +0000

    task answer

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -6,21 +6,24 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	hash_t *node;
+	hash_node_t *node;
 	unsigned long int i;
 
 	if (ht == NULL)
 		return;
 	printf("{");
-	for (i = 0; i < ht->size && ht->array[i]; i++)
+	for (i = 0; i < ht->size; i++)
 	{
-		node = ht->array[i];
-		while (node)
+		if (ht->array[i])
 		{
-			printf("'%s': '%s'", node->key, node->value);
-			node = node->next;
-			if (ht->array[i + 1] != NULL)
-				printf(", ");
+			node = ht->array[i];
+			while (node)
+			{
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+				if (ht->array[i + 1] != NULL)
+					printf(", ");
+			}
 		}
 	}
 	printf("}\n");
