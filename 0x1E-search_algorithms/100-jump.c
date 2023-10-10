1bc321b0c767479750422d1f30ba6859e6ae0a11
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-10-10 16:38:58 +0000

    task answer

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -29,7 +29,7 @@ int jump_search(int *array, size_t size, int value)
 	{
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (array[i++] == value)
-			return (value);
+			return (array[i]);
 	}
 	return (-1);
 }
